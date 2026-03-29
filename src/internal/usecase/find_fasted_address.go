f2d74d7ce9dd26e62761ca80005cfc2ed21c99d7
Roger Cruz <92161038+RogerGCruz@users.noreply.github.com>
2026-03-28 21:09:23 -0300

    feat: add use case for cep utilization

---
+++ src/internal/usecase/find_fasted_address.go
@@ -0,0 +1,65 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/pos-goexpert-desafio2/pos-goexpert-desafio-multithreading/src/internal/domain"
+	"github.com/pos-goexpert-desafio2/pos-goexpert-desafio-multithreading/src/internal/ports"
+)
+
+var ErrTimeout = errors.New("timeout for all providers in the given time one second")
+
+type FindFastestAddressUseCase struct {
+	providers []ports.CEPProvider
+	timeout   time.Duration
+}
+
+func NewFindFastestAddress(providers []ports.CEPProvider, timeout time.Duration) *FindFastestAddressUseCase {
+	return &FindFastestAddressUseCase{
+		providers: providers,
+		timeout:   timeout,
+	}
+}
+
+type result struct {
+	providerName domain.Address
+	source       string
+	err          error
+}
+
+func (uc *FindFastestAddressUseCase) Execute(ctx context.Context, cep string) (domain.Address, string, error) {
+	ctx, cancel := context.WithTimeout(ctx, uc.timeout)
+	defer cancel()
+
+	ch := make(chan result, len(uc.providers))
+
+	for _, provider := range uc.providers {
+		p := provider
+		go func() {
+			addr, err := p.Lookup(ctx, cep)
+			ch <- result{
+				providerName: addr,
+				source:       p.Name(),
+				err:          err,
+			}
+		}()
+	}
+
+	var errs []error
+	for i := 0; i < len(uc.providers); i++ {
+		select {
+		case res := <-ch:
+			if res.err == nil {
+				return res.providerName, res.source, nil
+			}
+			errs = append(errs, fmt.Errorf("%s: %w", res.source, res.err))
+		case <-ctx.Done():
+			return domain.Address{}, "", ErrTimeout
+		}
+	}
+	return domain.Address{}, "", errors.Join(errs...)
+
+}
